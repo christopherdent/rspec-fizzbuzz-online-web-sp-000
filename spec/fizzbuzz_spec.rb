bc70a58869772c9facc9fefb4236b9a3f674f97d
Christopher Dent <christopherdent01@gmail.com>
2019-07-05 02:26:18 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,3 +1,14 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb' 
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
+def fizzbuzz(number)
+
+if #{number} % 3 == 0
+puts "Fizz"
+elsif #{number} % 5 == 0
+puts "Buzz"
+elsif #{number} % 3 == 0 % #{number} % 5 == 0
+puts "FizzBuzz"
+else
+nil
+end
