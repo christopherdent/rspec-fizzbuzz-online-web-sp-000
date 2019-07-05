57242f15f1e982caa3030cd7f2e30857fe05d787
Christopher Dent <christopherdent01@gmail.com>
2019-07-05 02:37:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ if number % 3 == 0
 puts "Fizz"
 elsif number % 5 == 0
 puts "Buzz"
-elsif number % 3 == 0 % #{number} % 5 == 0
+elsif number % 3 == 0 % && number % 5 == 0
 puts "FizzBuzz"
 else
 nil
